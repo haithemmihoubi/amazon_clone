6753c11253f93e5a947f42338327138761845bcd
Haithem Mihoubi <64318421+haithemmihoubi@users.noreply.github.com>
2022-08-11 22:27:02 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:amazon_clone/constants/global_variables.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp( MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(scaffoldBackgroundColor: GlobalVariables.backgroundColor,
+        appBarTheme:const AppBarTheme(
+          elevation: 0,
+
+        ),
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title:const Text('Flutter'),
+        ),
+        body:const Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
+
